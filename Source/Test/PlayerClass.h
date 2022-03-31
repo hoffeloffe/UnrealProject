8ee5cde40219365e634ff5bcc0dac9915ad15851
Hoffe <djkagemand1@gmail.com>
2022-03-31 12:07:37 +0200

    Unreal

---
+++ Source/Test/PlayerClass.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class TEST_API PlayerClass
+{
+public:
+	float health;
+	int damage;
+	PlayerClass();
+	~PlayerClass();
+};
